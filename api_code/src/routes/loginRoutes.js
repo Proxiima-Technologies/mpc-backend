fcb6325da949e8762d821158260b8c2c5ef43d6e
Nikhil-Chouhan <Nikhil-Chouhan@users.noreply.github.com>
2023-07-01 15:30:31 +0530

    changes

--- api_code/src/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _LoginController = require('../controllers/LoginController'); var _LoginController2 = _interopRequireDefault(_LoginController);
-
-const router = new (0, _express.Router)()
-
-router.post('/', _LoginController2.default.store)
-
-exports. default = router
